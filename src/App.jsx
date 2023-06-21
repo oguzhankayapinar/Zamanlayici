5eacf489f45482b443846a9e6df36fb8567308bb
Oguzhan Kayapinar <oguzhankayapinar@gmail.com>
2023-06-22 01:03:08 +0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown';
 import '@leenguyen/react-flip-clock-countdown/dist/index.css';
+import logo from './assets/murkaylogoo.png';
 
 function App() {
 
@@ -9,13 +10,16 @@ function App() {
   return (
     <section className="container">
       <div className="clock_content">
+        <img src={logo} alt="Logo" width={350} />
         <h1>Yakında Hizmetinizdeyiz</h1>
-        <h3>Takipte Kalın</h3>
+
+
         <FlipClockCountdown
           className='flip-clock'
-          to={new Date().getTime() + 24 * 3600 * 1000 + 5000}
+          to={new Date().getTime() + 48 * 3600 * 1000 + 5000}
           labels={["GÜN", "SAAT", "DAKİKA", "SANİYE"]}
           duration={0.5}
+          style={{ fccDigitBlockWidth: "20px" }}
         />
         {/* <button className="button">Notify Me</button> */}
       </div>
